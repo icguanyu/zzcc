2f86dc063dd757110336f3fc8cc0cd4fd5ff420c
icguanyu <icguanyu@gmail.com>
2018-08-20 23:23:37 +0800

    hide menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,14 +1,16 @@
 (function() {
   const $window = $(window);
+  const $hamburgur = $(".hamburgur");
+  const $m_menu = $('.mobile_menu')
   const $topinfo = $(".top_info");
   const $header = $("header");
   const $stu_quick_btn = $("#stu_quick_btn");
-  const $menu = $('.menu>ul>li')
-  const $dropdown = $('.dropdown')
-  
+  const $menu = $(".menu>ul>li");
+  const $dropdown = $(".dropdown");
+
   let timer;
 
-  function toggleMenu(){
+  function toggleMenu() {
     if (timer) {
       clearTimeout(timer);
     }
@@ -23,35 +25,38 @@
       }
     }, 100);
   }
-  
 
-  function dropDown(){
-    let nowindex = $(this).index()
-    console.log(nowindex)
-    $dropdown.hide()
-    $dropdown.eq(nowindex).show()
+  function dropDown() {
+    let nowindex = $(this).index();
+    console.log(nowindex);
+    $dropdown.hide();
+    $dropdown.eq(nowindex).show();
   }
-  function hideList(){
-    $dropdown.hide()
+  function hideList() {
+    $dropdown.hide();
   }
 
   //手機選單
-$('.hamburgur').on('click',function(e){
-  $(this).children().toggleClass('nav_open')
-  $('.nav ul').toggleClass('active')
-  e.stopPropagation()
-})
-$(document).on('click', function() {
-  $('.hamburgur').children().removeClass('nav_open')
-  $('.nav ul').removeClass('active')
-})
+  $hamburgur.click(function(e) {
+    $(this)
+      .children()
+      .toggleClass('nav_open');
+    $m_menu.toggleClass('mobile_menu_open');
+    e.stopPropagation();
+  });
+  $(document).click(function() {
+    $hamburgur.children().removeClass('nav_open')
+    $m_menu.removeClass('mobile_menu_open')
+  });
 
   //Listener
   $window.scroll(toggleMenu);
-  $menu.mouseenter(dropDown)
-  $menu.mouseleave(hideList)
+  $menu.mouseenter(dropDown);
+  $menu.mouseleave(hideList);
 
   //Default
   $stu_quick_btn.hide();
   $dropdown.hide();
 })();
+
+addEventListener;
