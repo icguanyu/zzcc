8ea4b39a4cd90d435cf85dfcf526b1628a26268f
icguanyu <icguanyu@gmail.com>
2018-09-06 18:35:45 +0800

    filter+localstorage setting

--- assets/js/class.js
+++ assets/js/class.js
@@ -2,12 +2,19 @@ var app = new Vue({
   el: '.all',
   data: {
     data :[],
-    filter: 'all',
+    filter_keyword: '',
+    filter_kinds: 'all',
+    filter_weeks: [],
+    filter_period: [],
+    filter_other: [],
+    filter_favorite: false,
     storageArray: []
   },
   created() {
     const vm = this
     vm.getData()
+    //讀取localStorage
+    vm.storageArray = JSON.parse(localStorage.getItem('item'));
   },
   methods:{
     getData(){
@@ -17,10 +24,23 @@ var app = new Vue({
         if(res){
           res.forEach(item=>item['star'] = false)//加星星
           vm.data = res
+          if(!vm.storageArray) return
+          vm.storageArray.forEach((val)=>{
+            const hasStar = vm.data.find(item=>item.id===val)
+            hasStar['star'] = true
+          })
         }else{
           console.log('失敗')
         }
       })
+    },
+    addFavorite: function(id){
+      const vm = this
+      const target = vm.data.find(item=>item.id===id)
+      target.star = !target.star
+      vm.storageArray = vm.favorite.map((item)=>item.id)
+      //存localStorage
+      localStorage.setItem('item',JSON.stringify(vm.storageArray))
     }
   },
   computed:{
@@ -31,10 +51,45 @@ var app = new Vue({
     },
     filterClass(){
       const vm = this
+      //過濾條件
+      const weekReg = new RegExp(
+        this.filter_weeks.length !==0 ? `[${this.filter_weeks.join('')}@]` : `[一二三四五@]`,'gi'
+      )
+      const periodReg = new RegExp(
+        this.filter_period.length !==0 ? `[${this.filter_period.join('')}@]` : `[上下晚@]`
+        ,'gi'
+      )
+      const otherReg = new RegExp(
+        this.filter_other.length !==0 ? `[${this.filter_other.join('')}@]` : `.*`
+        ,'gi'
+      )
       const result = vm.data.filter((item)=>{
-        return vm.filter==='all'?item:item.kinds_1 === vm.filter
+        return (
+          vm.filter_kinds === 'all' 
+          ? 
+          item.week.match(weekReg) &&
+          item.period.split('')[0].match(periodReg) &&
+          item.class_name.match(otherReg) &&
+          item.class_name.match(this.filter_keyword)
+          :
+          item.kinds_1 === vm.filter_kinds &&
+          item.week.match(weekReg) && 
+          item.period.split('')[0].match(periodReg) &&
+          item.class_name.match(otherReg) &&
+          item.class_name.match(this.filter_keyword)
+        )
       })
+      return result
+    },
+    favorite(){
+      const vm = this
+      const result = vm.data.filter((item)=>item.star)
       return result
     }
   }
 })
+
+// $('.favorite_icon').click(function(){
+//   $('.search_result').toggleClass('hide_result')
+//   $('.favorite_box').delay(600).toggleClass('show_favorite')
+// })
