13a15ea32775cd61c3f47abf3dfc384f0a4c5d8d
icguanyu <icguanyu@gmail.com>
2018-09-10 01:56:39 +0800

    add news slide rwd

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,31 +1,51 @@
 let now = 0
 const $silde = $('.slide')
-
+//首頁封面slide
 setInterval(function(){
   now = (now + 1)%3
   $silde.fadeOut().eq(now).fadeIn()
 },5000)
 
-let $next = $('.next')
-let $prev = $('.prev')
-let $block = $('.block')
-let $news_list = $('.news_list')
-let $news_box = $('.news_box')
+//新聞slide
+let counts //視窗顯示數量
+const $news_box = $('.news_box')
+const $news_list = $('.news_list')
+const $block = $('.block')
+const length = $block.length
+const move = $block.outerWidth(true) //block寬
+const containWidth = $block.length*move*2//兩倍數量的寬(因複製)
+const star = $('.block').slice(-1).clone() //前面補上尾巴1個
+const end = $('.block').slice(0,length-1).clone() //後面補上剩下的
 
-
-function newsSlide(){
-  const move = $block.outerWidth()
-  if($(this).is($next)){
-    $news_list.stop().animate({left:move*-1},400,function(){
-      $(this).css({left:0})
-      $('.block').eq(0).appendTo($news_list)
+function handler(){
+  if($(this).is('.next')){
+    $news_list.stop().animate({left:(move*-1) - move},400,()=>{
+      $news_list.css({left:move*-1})
+      $('.news_list .block').eq(0).appendTo($news_list)
     })
   }else{
-    $news_list.stop().animate({left:move},400,function(){
-      $(this).css({left:0})
-      $('.block').eq(-1).prependTo($news_list)
+    $news_list.stop().animate({left:0},400,()=>{
+      $news_list.css({left:move*-1})
+      $('.news_list .block').eq(-1).prependTo($news_list)
     })
-  }
+  } 
 }
-$next.click(newsSlide)
-$prev.click(newsSlide)
+//判斷視窗寬度決定顯示個數
+function checkWidth(){
+  let w = $(window).width()
+  if(w<=468){
+    counts = 1
+  }else if(w<=768){
+    counts = 2
+  }else if(w<=1024){
+    counts = 3
+  }else{
+    counts = 4
+  }
+  $news_box.css({width:move*counts})
+}
+$news_list.append(end).prepend(star).css({width:containWidth,left:move*-1})
+
+$(window).on('load',checkWidth)
+$(window).on('resize',checkWidth)
+$('.prev,.next').click(handler)  
